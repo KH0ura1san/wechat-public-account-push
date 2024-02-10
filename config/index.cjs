132fa993318f9f5bff8b3d88e513d6f551d608d8
KH0ura1san <73892092+KH0ura1san@users.noreply.github.com>
2024-02-11 02:25:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,38 +6,39 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-      // 我们在一起已经有xxxx天了的配置
-      "IS_SHOW_COLOR":true,
-      "USERS":[
-        {
-          "name":"佳宏宝儿","id":"85f709eedc7c84bbd7cc112c95e576ff",
-          "useTemplateId":"0001",
-          "province":"浙江",
-          "city":"湖州",
-          "horoscopeDate":"01-10",
-          "horoscopeDateType":"今日",
-          "openUrl":"https://shuangxunian.github.io/",
-          "festivals":[
-            {"type":"生日","name":"宝儿的生日","date":"01-10","year":"2001"},
-            {"type":"节日","name":"恋爱纪念日","date":"02-28","year":"2021"},
-            {"type":"节日","name":"相识纪念日","date":"11-03","year":"2020"}
-          ],
-          customizedDateList: [
-            // 在一起的日子
-            { keyword: 'love_day', date: '2021-02-28' },
-          ],
-        "SWITCH":
-          {"weather":true,
-           "holidaytts":true,
-           "CIBA":false,
-           "oneTalk":false,
-           "earthyLoveWords":true,
-           "momentCopyrighting":false,
-           "poisonChickenSoup":false,
-           "poetry":false,
-           "horoscope":true,
-           "birthdayMessage":true}
-}
+    // 我们在一起已经有xxxx天了的配置
+    "IS_SHOW_COLOR":true,
+    "USERS":[
+      {
+        "name":"佳宏宝儿","id":"85f709eedc7c84bbd7cc112c95e576ff",
+        "useTemplateId":"0001",
+        "province":"浙江",
+        "city":"湖州",
+        "horoscopeDate":"01-10",
+        "horoscopeDateType":"今日",
+        "openUrl":"https://shuangxunian.github.io/",
+        "festivals":[
+          {"type":"生日","name":"宝儿的生日","date":"01-10","year":"2001"},
+          {"type":"节日","name":"恋爱纪念日","date":"02-28","year":"2021"},
+          {"type":"节日","name":"相识纪念日","date":"11-03","year":"2020"}
+        ],
+        customizedDateList: [
+          // 在一起的日子
+          { keyword: 'love_day', date: '2021-02-28' },
+        ],
+      "SWITCH":
+        {"weather":true,
+         "holidaytts":true,
+         "CIBA":false,
+         "oneTalk":false,
+         "earthyLoveWords":true,
+         "momentCopyrighting":false,
+         "poisonChickenSoup":false,
+         "poetry":false,
+         "horoscope":true,
+         "birthdayMessage":true}
+      }]}
+
 
 module.exports = USER_CONFIG
 
