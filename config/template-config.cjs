0f11288bfda177c538846d1b15b9b4993e35ac90
KH0ura1san <73892092+KH0ura1san@users.noreply.github.com>
2024-02-11 09:44:06 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -4,16 +4,17 @@ const TEMPLATE_CONFIG = [
     id: '0001',
     title: '我可爱的佳宏宝~',
     desc: `
-      今天是{{date.DATA}}
-      {{holidaytts.DATA}}
-      你现在在{{city.DATA}}
-      今天的天气 {{weather.DATA}}
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      {{comprehensive_horoscope.DATA}}
-      今天是我们相识的第{{meet_day.DATA}}天
-      我们已经在一起{{love_day.DATA}}天啦
-      {{birthday_message.DATA}}
-      {{moment_copyrighting.DATA}}
+      今天是{{date.DATA}}\n
+      {{holidaytts.DATA}}\n
+      你现在在{{city.DATA}}\n
+      今天的天气 {{weather.DATA}}\n
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}\n
+      要注意穿衣喔，不要着凉了\n
+      {{comprehensive_horoscope.DATA}}\n
+      今天是我们相识的第{{meet_day.DATA}}天\n
+      我们已经在一起{{love_day.DATA}}天啦\n
+      {{birthday_message.DATA}}\n
+      {{moment_copyrighting.DATA}}\n
     `
   },
   {
